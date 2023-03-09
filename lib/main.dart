f3c98007f46fe9beab94d111701df524d76bdb41
BasakK6 <katukbasak@gmail.com>
2023-03-09 15:07:18 +0300

    feature: made the app interactive by following a story plan

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:destini_flutter/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(const Destini());
 
 class Destini extends StatelessWidget {
@@ -58,11 +56,12 @@ class _StoryPageState extends State<StoryPage> {
                 child: TextButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     storyBrain.getChoice1(),
                     style: const TextStyle(
                       fontSize: 20.0,
@@ -81,7 +80,9 @@ class _StoryPageState extends State<StoryPage> {
                 child: TextButton(
                   onPressed: () {
                     //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(2);
+                    });
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.blue),
                   child: Text(
